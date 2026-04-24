4ad5769ff1073152c5d99d2e180686cf17264fe3
lingling5400 <136593138+lingling5400@users.noreply.github.com>
2026-04-24 14:59:23 +0800

    Update 氣泡排序.java

--- 氣泡排序.java
+++ 氣泡排序.java
@@ -4,8 +4,8 @@ class Solution {
     public int[] sortDesc(int[] c) {
         int n = c.length;
         
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
+        for (int i = 0; i < n - 1; i++) {           // 幾輪 ; n-1:因為要用 j+1，所以不能跑到最後一格
+            for (int j = 0; j < n - 1 - i; j++) {   // 每輪掃多遠 ; j < n - 1 - i:避開後面牌號的數字，減少時間浪費
                 
                 if (c[j] < c[j + 1]) {  // 大到小
                     int temp = c[j];
@@ -18,3 +18,22 @@ class Solution {
         return c;
     }
 }
+
+
+/* 
+//Arrays.sort(c)
+import java.util.Arrays;
+Arrays.sort(c);   // 由小到大排序
+
+//由大到小排序
+Arrays.sort(c);
+
+for (int i = 0; i < c.length / 2; i++) {
+    int temp = c[i];
+    c[i] = c[c.length - 1 - i];
+    c[c.length - 1 - i] = temp;
+}
+//時間複雜度：O(n log n)
+//Java 內建高度優化（通常是 QuickSort / TimSort）
+//實務使用標準解
+*/
